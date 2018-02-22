2599eaafe69eaf83fbf5cba35c55c96c91b98c74
Kangyroo <ianwazzup139@gmail.com>
2018-02-21 19:34:48 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,53 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log(`Baymax has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+
+  client.user.setGame('with Kang ;)');
+
+});
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply('Pong!');
+  }
+});
+
+client.on("message", msg => {
+    if (msg.content === "THE TING GOES") {
+      msg.reply('SKRAAA');
+    }
+  });
+ 
+  client.on("message", msg => {
+    if (msg.content === "Is Zen gay?") {
+      msg.reply('Yes.');
+    }  
+  });
+
+  client.on("message", msg => {
+    if (msg.content === "Blox") {
+      msg.reply('Blox The Rebel.');
+    }  
+  });
+
+  client.on("message", msg => {
+    if (msg.content === "Baymax") {
+      msg.reply('What you want bitch.');
+    }  
+  });
+
+  client.on("message", msg => {
+    if (msg.content === "Say something") {
+      msg.reply('IM GIVING UP ON YOU!!');
+    }  
+  });
+
+  client.on("message", msg => {
+    if (msg.content === "Pray your gay away") {
+      msg.reply("https://queerty-prodweb.s3.amazonaws.com/wp/docs/2012/10/pray-your-gay-away.jpg");
+    }
+  })
+  
+client.login('Bot_TOKEN')
