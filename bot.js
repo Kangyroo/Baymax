815ae9269eb558a91d707abd3b9b016b772d1bac
Kangyroo <ianwazzup139@gmail.com>
2018-02-21 20:01:33 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on("ready", () => {
     console.log(`Baymax has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 
-  client.user.setGame('with Kang ;)');
+  client.user.setGame('with Kang');
 
 });
 
